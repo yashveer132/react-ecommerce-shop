1d54efbab6ab3c8850256c38a5a67a1ad1f4d427
Yash <ys3792@srmist.edu.in>
2023-01-08 20:05:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
+
+function App() {
+  return (
+    <div className="App">
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/products.js
@@ -0,0 +1,54 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Stylish HAT",
+    price: 40.0,
+    productImage: product8,
+  },
+  {
+    id: 2,
+    productName: "LED Light Strips",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 3,
+    productName: "GOLF SERVICE SHIRT",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 4,
+    productName: " Denim Jacket",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "IPhone XS",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 6,
+    productName: "Rainbow TEE",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Cannon M65 Camera",
+    price: 699.0,
+    productImage: product3,
+  },
+  
+];
